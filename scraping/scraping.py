0490bcf264bc3dd04cda68ea32b9f47957291408
OnedariDaisakusen <65679065+OnedariDaisakusen@users.noreply.github.com>
2023-11-13 15:37:43 -0500

    pruebas

--- scraping/scraping.py
+++ scraping/scraping.py
@@ -11,8 +11,8 @@ from datetime import datetime
 resultado = {}
 
 def iniciarProceso():
-
-    # user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36"
+    PROXY = "123.30.154.171:7777"
+    user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36"
 
     #Usamos firefox como navegador
     # options = webdriver.FirefoxOptions()
@@ -21,17 +21,18 @@ def iniciarProceso():
     options = webdriver.ChromeOptions()
 
     # options.add_argument('--headless')
-    #options.add_argument(f'user-agent={user_agent}')
-    # options.add_argument("--window-size=1920,1080")
-    # options.add_argument('--ignore-certificate-errors')
-    # options.add_argument('--allow-running-insecure-content')
-    # options.add_argument("--disable-extensions")
+    options.add_argument(f'user-agent={user_agent}')
+    options.add_argument("--window-size=1920,1080")
+    options.add_argument('--ignore-certificate-errors')
+    options.add_argument('--allow-running-insecure-content')
+    options.add_argument("--disable-extensions")
     # options.add_argument("--proxy-server='direct://'")
-    # options.add_argument("--proxy-bypass-list=*")
-    # options.add_argument("--start-maximized")
-    # options.add_argument('--disable-gpu')
-    # options.add_argument('--disable-dev-shm-usage')
-    # options.add_argument('--no-sandbox')
+    options.add_argument('--proxy-server=%s' % PROXY)    
+    options.add_argument("--proxy-bypass-list=*")
+    options.add_argument("--start-maximized")
+    options.add_argument('--disable-gpu')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--no-sandbox')
 
     # Se agregan opciones si son necesarias
     # driver = webdriver.Firefox(options=options)
@@ -50,7 +51,8 @@ def iniciarProceso():
     imput_ruc = driver.find_element(By.ID, 'txtRuc')
     # Le digito un valor
     time.sleep(random.randint(1, 10))
-    imput_ruc.send_keys('20487988023')
+    # imput_ruc.send_keys('20487988023')
+    imput_ruc.send_keys('20469378561')
 
     # Buscamos el boton para el submit
     boton_buscar = driver.find_element(By.ID, 'btnAceptar')
@@ -65,58 +67,98 @@ def iniciarProceso():
         tabla_resultado = driver.find_element(By.CLASS_NAME, 'list-group') 
         # Aqui se obtiene la lista de items que tiene el contendor
         filas_resultado = tabla_resultado.find_elements(By.CLASS_NAME, 'list-group-item')
-
+        cod_result_bloque1 = 0
+        cod_result_bloque2 = 0
+        cod_result_bloque3 = 0
+       
         for fila in filas_resultado:
             try:
                 # Cada list-group-item puede tener uno o mas rows#driver.quit()
                 elemento_row = fila.find_element(By.CLASS_NAME, "row")
-                try:
-                    nombre_valor = elemento_row.find_element(By.CLASS_NAME, "col-sm-5")
-                    nombre_valor_text = nombre_valor.text.replace(':', '')
+                # print('Se ingreso en el bloque 1')
+                # print('elemento_row', elemento_row.text)
 
-                    valor = elemento_row.find_element(By.CLASS_NAME, "col-sm-7")
-                    valor_text = valor.text
+                # # Este bloque try obtiene de la etiqueta row las etiquetas col-sm-5 / col-sm-7
+                cod_result_bloque1 = obtenerValoresComunesBloque1(elemento_row)
 
-                    resultado[mapeo_resultado(nombre_valor_text)] = valor_text
+                if cod_result_bloque1 == 1:
+                    #cod_result_bloque2 = obtenerValoresComunesBloque2(elemento_row)
+                    #print('Se ingreso en el bloque 2')
+                    #print(elemento_row.text)
+                    nombre_valor = elemento_row.find_element(By.CLASS_NAME, "col-sm-3")
+                    print('col-sm-3 - campos',nombre_valor.text )
 
-                except NoSuchElementException:
-                    print("No se encontró el elemento col-sm-5 / col-sm-7 en esta fila.")            
-                    #driver.quit()
+                # if cod_result_bloque2 == 1:
+                #     print('Se ingreso en el bloque 3')
+                # # Este bloque try obtiene de la etiqueta row las etiquetas col-sm-5 / col-sm-7
+                # try:
+                #     nombre_valor = elemento_row.find_element(By.CLASS_NAME, "col-sm-5")
+                #     nombre_valor_text = nombre_valor.text.replace(':', '')
+
+                #     valor = elemento_row.find_element(By.CLASS_NAME, "col-sm-7")
+                #     valor_text = valor.text
+
+                #     resultado[mapeo_resultado(nombre_valor_text)] = valor_text
+                # except NoSuchElementException:
+                #     print("No se encontró el elemento col-sm-5 / col-sm-7 en esta fila.")
+                #     # Este bloque try obtiene de la etiqueta row las etiquetas col-sm-3, que es exclusivo del campo Fecha de Inscripción:/ Fecha de Inicio de Actividades:
+                #     try:
+                #         fechas_ins_act = elemento_row.find_element(By.CLASS_NAME, "col-sm-3")
+                #         print('Aqui esta el elemento que buscas: ', fechas_ins_act.text)
+                #         # for fecha in fechas_ins_act:
+                #         #     fecha_h4 = fecha.find_element(By.TAG_NAME, "h4")
+                #         #     fecha_h4_text = fecha_h4.text.replace(':', '')
+
+                #         #     fecha_p = fecha.find_element(By.TAG_NAME, "p")
+                #         #     fecha_p_text = fecha_p.text.replace(':', '')                        
+
+                #         #     resultado[mapeo_resultado(fecha_h4_text)] = fecha_p_text
+
+                #     except NoSuchElementException:
+                #         print("No se encontró el elemento col-sm-3 en esta fila.")   
+
+               
             except NoSuchElementException:
                 print("No se encontró el elemento row en esta fila.")
-                #driver.quit()
-        agregar_valores_defecto('20487988023')
-        print(resultado)
-        insertar_resultado(resultado)                            
+            cod_result_bloque1 = 0
+            cod_result_bloque2 = 0
+            cod_result_bloque3 = 0
+        #agregar_valores_defecto('20487988023')
+        #print(resultado)
+        #insertar_resultado(resultado)                            
     except NoSuchElementException: 
-        print("Hubo un error al obtener el elemento") 
+        print("Hubo un error al obtener el elemento list-group") 
         #driver.quit()
 
     #driver.quit()  
-    time.sleep(random.randint(1, 10))
+    # time.sleep(random.randint(1, 10))
     driver.quit()  
 
- 
 
 
-# def mapeoVariables(nombreValor, valor):
-#     if nombreValor == 'Número de RUC:':
-#         resultado[nombreValor] = valor
-#     if nombreValor == 'Tipo Contribuyente:':
-#         resultado[nombreValor] = valor        
+def obtenerValoresComunesBloque1(row):
+    try:
+        nombre_valor = row.find_element(By.CLASS_NAME, "col-sm-5")
+        nombre_valor_text = nombre_valor.text.replace(':', '')
 
-# def insertar_resultado(resultado):
-#     # Establecer conexión y obtener el cursor
-#     conexion, cursor = conectar_bd()
+        valor = row.find_element(By.CLASS_NAME, "col-sm-7")
+        valor_text = valor.text
 
-#     # Llamamos a la función para insertar los datos
-#     insertar_resultado(cursor, resultado)
-
-#     # Confirmar la transacción y cerrar el cursor y la conexión
-#     conexion.commit()
-#     cursor.close()
-#     conexion.close()
-
+        resultado[mapeo_resultado(nombre_valor_text)] = valor_text
+        return 0 # Si todo es correcto
+    except NoSuchElementException:
+        print("No se encontró el elemento row en esta fila.")
+        return 1 # Si hubo un error
+    
+def obtenerValoresComunesBloque2(row):
+    try:
+        nombre_valor = row.find_element(By.CLASS_NAME, "col-sm-3")
+        print('col-sm-3 - campos',nombre_valor.text )
+        return 0 # Si todo es correcto
+    except NoSuchElementException:
+        print("No se encontró el elemento col-sm-3 en esta fila.")
+        return 1 # Si hubo un error    
+    
 def agregar_valores_defecto(ruc):
     resultado['fechaBusqueda'] = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     resultado['numeroRuc'] = ruc
@@ -126,9 +168,6 @@ def agregar_valores_defecto(ruc):
     resultado['actividadComercioInterior'] = ''
     resultado['importante'] = ''
     
-
-    
-
 def mapeo_resultado(nombreValor):
     if nombreValor == 'Número de RUC':
         nombreValor = 'razonSocial'
@@ -165,4 +204,7 @@ def mapeo_resultado(nombreValor):
     elif nombreValor == 'Importante':
         nombreValor = 'importante'                                                                            
     return nombreValor    
+
+
+
 iniciarProceso()
